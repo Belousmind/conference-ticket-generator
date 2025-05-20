36829d822d08d11670f1d9ad1b660af05429ba7c
belousmind <belousmind@gmail.com>
2025-05-20 11:29:31 +0300

    refactor: styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,25 @@ import FormPage from '@components/form-page';
 import Ticket from '@components/ticket';
 import { useTicketStore } from './store/ticket-store';
 import Header from '@components/header';
-import img from './public/image-avatar.jpg'
+
 export default function App() {
   const ticket = useTicketStore((state) => state.ticket);
 
   return (
     <>
       <Header />
-      <FormPage />
-      <Ticket
-        name="Ada Lovelace"
-        email="ada.lovelace@computing.io"
-        github="@ada-codequeen"
-        imgSrc={img}
-      />
-      {/* {ticket ? (
-        <Ticket
-          name={ticket.name}
-          email={ticket.email}
-          github={ticket.github}
-          imgSrc={ticket.imageUrl}
-        />
-      ) : (
-        <FormPage />
-      )} */}
+      <main>
+        {ticket ? (
+          <Ticket
+            name={ticket.name}
+            email={ticket.email}
+            github={ticket.github}
+            imgSrc={ticket.imageUrl}
+          />
+        ) : (
+          <FormPage />
+        )}
+      </main>
     </>
   );
 }
